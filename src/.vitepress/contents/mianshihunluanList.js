8d0cd74aea429c60328d48a9173abe040b752720
wangguohan <guohan.wang@eoitek.com>
2023-10-10 16:58:30 +0800

    xss-crsf 和面试题

--- src/.vitepress/contents/mianshihunluanList.js
+++ src/.vitepress/contents/mianshihunluanList.js
@@ -2,5 +2,7 @@ module.exports = [
     { text: '快速开始', link: '/mianshi/hunluan/quiteStart.md' },
     { text: '移动端点击延迟及解决方案', link: '/mianshi/hunluan/mobile-delay-plan.md' },
     { text: '计算机网络面试题图片', link: '/mianshi/hunluan/net-audition-subject.md' },
-    {text:'浏览器调用本地软件',link: '/mianshi/hunluan/window-native.md' }
+    {text:'浏览器调用本地软件',link: '/mianshi/hunluan/window-native.md' },
+    {text:'XSS、CSRF攻击以及预防手段',link: '/mianshi/hunluan/xss-crsf.md' },
+    {text:'面试题总结',link: '/mianshi/hunluan/mianshi1.md' }
 ];
