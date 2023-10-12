9c0e2cc8c0edb6faa41bf04207b642bc3244a959
wangguohan <guohan.wang@eoitek.com>
2023-10-12 15:36:38 +0800

    增减面试菜单

--- src/.vitepress/contents/mianshihunluanList.js
+++ src/.vitepress/contents/mianshihunluanList.js
@@ -5,5 +5,7 @@ module.exports = [
     {text:'浏览器调用本地软件',link: '/mianshi/hunluan/window-native.md' },
     {text:'XSS、CSRF攻击以及预防手段',link: '/mianshi/hunluan/xss-crsf.md' },
     {text:'面试题总结',link: '/mianshi/hunluan/mianshi1.md' },
-    {text:'浏览器工作原理与实践',link:'/mianshi/hunluan/browser-principle.md'}
+    {text:'浏览器工作原理与实践',link:'/mianshi/hunluan/browser-principle.md'},
+    {text:'【图解 Google V8】学习笔记合集 23 篇（完结）',link:'/mianshi/hunluan/tujie-google-v8.md'},
+    {text:'vue3企业级项目实战',link:'/mianshi/hunluan/vue3企业级项目实战.md'},
 ];
