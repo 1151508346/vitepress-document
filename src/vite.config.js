2bc870acbc34f6f68a217c4c31666b05295dee0b
wangguohan <guohan.wang@eoitek.com>
2024-09-24 20:45:31 +0800

    feat: 添加离线搜索功能

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from "vite";
 
 import DefineOptions from 'unplugin-vue-define-options/vite'
 import requireTransform from 'vite-plugin-require-transform'
+import { SearchPlugin } from '@pzy915/vitepress-plugin-search'
+// import { load, cut } from '@node-rs/jieba'
 export default defineConfig({
   plugins:[
     DefineOptions(),
     // requireTransform({
     //   fileRegex:/.js$|.vue$/
     // })
+    SearchPlugin({
+      previewLength: 20,
+      buttonLabel: '搜索',
+      placeholder: '文章搜索',
+      tokenize: 'full',
+    }),
   ]
 })
