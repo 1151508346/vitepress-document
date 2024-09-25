c9d94863f23815b7c8658463300ab2370e317489
wangguohan <guohan.wang@eoitek.com>
2024-09-25 22:41:04 +0800

    升级vitepress

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
-
+import path from 'path'
 import DefineOptions from 'unplugin-vue-define-options/vite'
 import requireTransform from 'vite-plugin-require-transform'
 import { SearchPlugin } from '@pzy915/vitepress-plugin-search'
 // import { load, cut } from '@node-rs/jieba'
+function errorPlugin(){
+  return {
+    apply:'build',
+    enfore: 'pre',
+    resolveId(id){
+      if(id.includes('vite/preload-helper')) return 'vite/preload-helper'
+      if (id.startsWith('@/')) {
+        const base = path.resolve(process.cwd(), 'src')
+        return id.replace(/@/,base)
+      }
+    }
+  }
+}
 export default defineConfig({
   plugins:[
     DefineOptions(),
@@ -16,5 +29,6 @@ export default defineConfig({
       placeholder: '文章搜索',
       tokenize: 'full',
     }),
+    // errorPlugin()
   ]
 })
