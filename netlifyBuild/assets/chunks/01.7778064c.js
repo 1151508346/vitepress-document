67aa278ba5947be1408d4a31fb60e9d1a69a7778
wangguohan <guohan.wang@eoitek.com>
2024-09-26 20:23:54 +0800

    fix: modify build

--- netlifyBuild/assets/chunks/01.7778064c.js
+++
@@ -1,1 +0,0 @@
-var a="/assets/01.13c59aab.jpg";export{a as _};
--- netlifyBuild/assets/chunks/path-gradle.978a580b.js
+++
@@ -1,1 +0,0 @@
-var s="/assets/path-java.1c7454f6.png",a="/assets/sdk-platform.cb5b077b.png",t="/assets/android-build-tools.4f14269a.png",p="/assets/path-gradle.0e812e25.png";export{s as _,a,t as b,p as c};
